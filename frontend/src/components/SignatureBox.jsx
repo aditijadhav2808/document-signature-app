67fb31f415c657aa1d5b143befc4beab17e54daa
Aditi <aditijadhav2808@gmail.com>
2026-06-19 11:57:49 +0530

    Added draggable signature feature

--- frontend/src/components/SignatureBox.jsx
+++ frontend/src/components/SignatureBox.jsx
@@ -1,25 +1,40 @@
-export default function SignatureBox({
-  position,
-}) {
+import { useRef } from "react";
+import Draggable from "react-draggable";
+
+export default function SignatureBox({ position, setPosition }) {
+  const nodeRef = useRef(null);
+
   return (
-    <div
-      style={{
-        position: "absolute",
-        left: position.x,
-        top: position.y,
-
-        color: "black",
-        fontSize: "28px",
-        fontFamily: "cursive",
-        fontStyle: "italic",
-        fontWeight: "bold",
-
-        cursor: "move",
-        zIndex: 9999,
-        userSelect: "none",
+    <Draggable
+      nodeRef={nodeRef}
+      position={position}
+      onStop={(e, data) => {
+        setPosition({
+          x: data.x,
+          y: data.y,
+        });
       }}
     >
-      𝓐𝓭𝓲𝓽𝓲 𝓙𝓪𝓭𝓱𝓪𝓿
-    </div>
+      <div
+        ref={nodeRef}
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "160px",
+          height: "50px",
+          backgroundColor: "yellow",
+          border: "2px solid black",
+          cursor: "move",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          zIndex: 10,
+          fontWeight: "bold",
+        }}
+      >
+        Sign Here
+      </div>
+    </Draggable>
   );
 }
